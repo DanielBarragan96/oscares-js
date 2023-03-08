fd54cc56bc89f78dfd975728e419cb84aa4e196b
DanielBarragan96 <ie702554@iteso.mx>
2023-03-08 10:54:03 -0600

    implementing results table and storetime on submit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,46 @@ import Category from "./Category";
 import oscar from "./oscar.png";
 
 import nominations from "./nominations.json";
+import { useState } from "react";
 
 function App() {
+  let [submitted, setSubmitted] = useState(
+    localStorage.getItem("submitted") !== null
+  );
+
+  function submitVote() {
+    setSubmitted(true);
+    let newDate = new Date();
+    localStorage.setItem("submitted", newDate);
+    window.scrollTo(0, 0);
+    window.location.reload(true);
+  }
+
   return (
     <div className="App">
       <img src={oscar} alt="oscar" />
       <h1>95th Academy Awards</h1>
+      {submitted ? (
+        <>
+          <table key={submitted}>
+            <tbody>
+              {nominations.map((nomination, i) => (
+                <tr key={i}>
+                  <td>
+                    <label>{nomination.title}</label>
+                  </td>
+                  <td>
+                    <label>{localStorage.getItem(nomination.title)}</label>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <h4>{localStorage.getItem("submitted")}</h4>
+        </>
+      ) : (
+        <div></div>
+      )}
       {nominations.map((nomination, i) => (
         <Category
           title={nomination.title}
@@ -16,6 +50,7 @@ function App() {
           key={i}
         />
       ))}
+      <button onClick={submitVote}>Submit</button>
     </div>
   );
 }
