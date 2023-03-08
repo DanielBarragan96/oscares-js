eb615d19ea1d3b597d75248bfcec35139e613070
DanielBarragan96 <ie702554@iteso.mx>
2023-03-07 18:04:56 -0600

    imported nomination

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import "./App.css";
 import Category from "./Category";
 
-const nominations = [
-  {
-    title: "ACTOR IN A LEADING ROLE",
-    nominees: [
-      "AUSTIN BUTLER, Elvis",
-      "COLIN FARRELL, The Banshees of Inisherin",
-      "BRENDAN FRASER, The Whale",
-      "PAUL MESCAL, Aftersun",
-      "BILL NIGHY, Living",
-    ],
-  },
-];
+import nominations from "./nominations.json";
 
 function App() {
   return (
     <div className="App">
+      <h1>95th Academy Awards</h1>
       {nominations.map((nomination, i) => (
         <Category
           title={nomination.title}
