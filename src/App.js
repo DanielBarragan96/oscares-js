ec27af01ae53ce0ce614bc744224d1c569fdc68a
DanielBarragan96 <ie702554@iteso.mx>
2023-03-07 18:07:35 -0600

    display oscar image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Category from "./Category";
+import oscar from "./oscar.png";
 
 import nominations from "./nominations.json";
 
 function App() {
   return (
     <div className="App">
+      <img src={oscar} alt="oscar" />
       <h1>95th Academy Awards</h1>
       {nominations.map((nomination, i) => (
         <Category
